ec47a0d41dc9e830c7c235f2ed045cb492b8bb7f
mintball9543 <minseokcomputer@gmail.com>
2025-05-25 20:36:49 +0900

    Q32

--- src/main/java/com/example/jpa/user/controller/ApiUserController.java
+++ src/main/java/com/example/jpa/user/controller/ApiUserController.java
@@ -1,8 +1,10 @@
 package com.example.jpa.user.controller;
 
 import com.example.jpa.notice.model.ResponseError;
+import com.example.jpa.user.entity.User;
 import com.example.jpa.user.model.UserInput;
-import org.apache.coyote.Response;
+import com.example.jpa.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -15,20 +17,47 @@ import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class ApiUserController {
 
+    private final UserRepository userRepository;
+
+
+    // Q31
+//    @PostMapping("/api/user")
+//    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors){
+//        List<ResponseError> reponseErrorList = new ArrayList<>();
+//
+//        if(errors.hasErrors()){
+//            errors.getAllErrors().forEach((e) -> {
+//                reponseErrorList.add(ResponseError.of((FieldError)e));
+//            });
+//            return new ResponseEntity<>(reponseErrorList, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return new ResponseEntity<>(userInput, HttpStatus.OK);
+//    }
 
     @PostMapping("/api/user")
-    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors){
-        List<ResponseError> reponseErrorList = new ArrayList<>();
+    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+        List<ResponseError> responseErrorList = new ArrayList<>();
 
-        if(errors.hasErrors()){
+        if (errors.hasErrors()) {
             errors.getAllErrors().forEach((e) -> {
-                reponseErrorList.add(ResponseError.of((FieldError)e));
+                responseErrorList.add(ResponseError.of((FieldError) e));
             });
-            return new ResponseEntity<>(reponseErrorList, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
         }
+
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .userName(userInput.getUserName())
+                .password(userInput.getPassword())
+                .phone(userInput.getPhone())
+                .regDate(java.time.LocalDateTime.now())
+                .build();
+        userRepository.save(user);
 
         return new ResponseEntity<>(userInput, HttpStatus.OK);
     }
