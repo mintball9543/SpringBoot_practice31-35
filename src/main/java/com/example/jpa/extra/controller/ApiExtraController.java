4cb195369047c9b34e2cecba99a270b86402128e
mintball9543 <minseokcomputer@gmail.com>
2025-05-29 19:31:47 +0900

    Q88 85+시도/구군 단위 검색 기능

--- src/main/java/com/example/jpa/extra/controller/ApiExtraController.java
+++ src/main/java/com/example/jpa/extra/controller/ApiExtraController.java
@@ -2,34 +2,46 @@ package com.example.jpa.extra.controller;
 
 import com.example.jpa.common.model.ResponseResult;
 import com.example.jpa.extra.model.OpenApiResult;
+import com.example.jpa.extra.model.PharmacySearch;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.net.URLEncoder;
 import java.util.Collections;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ApiExtraController {
 
-    // Q86~87
+    // Q86~88
     @GetMapping("/api/extra/pharmacy")
-    public ResponseEntity<?> pharmacy() {
+    public ResponseEntity<?> pharmacy(@RequestBody PharmacySearch pharmacySearch) {
         String apiKey = "D3oa9VQ0e0tl8hkpoqTG9kLJWlUB%2FZmHdAZ0OWRPsS8PcY5Ft6PCx7pI2cgFkJS4z4tTs%2BHXcto50WqCrtlCPQ%3D%3D";
-        String url = "https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
+        String url = String.format("https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10", apiKey);
 
         String apiResult = "";
 
         try {
-            URI uri = new URI(String.format(url, apiKey));
+            url += String.format("&Q0=%s&Q1=%s"
+                    , URLEncoder.encode(pharmacySearch.getSearchSido(), "UTF-8")
+                    , URLEncoder.encode(pharmacySearch.getSearchGugun(), "UTF-8"));
+
+            URI uri = new URI(url);
+
+            log.info(url);
+
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
