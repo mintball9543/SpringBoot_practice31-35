3014291dd33e4478d75ea1cdcbaf04d66b24bb8a
mintball9543 <minseokcomputer@gmail.com>
2025-05-29 13:09:04 +0900

    Q84 로그인시 에러가 발생하는 경우 로그에 기록하는 기능

--- src/main/java/com/example/jpa/user/controller/ApiLoginController.java
+++ src/main/java/com/example/jpa/user/controller/ApiLoginController.java
@@ -9,6 +9,7 @@ import com.example.jpa.user.model.UserLoginToken;
 import com.example.jpa.user.service.UserService;
 import com.example.jpa.util.JWTUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -17,15 +18,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ApiLoginController {
 
     private final UserService userService;
 
+
     // Q83
+//    @PostMapping("/api/login")
+//    public ResponseEntity<?> login(@RequestBody @Valid UserLogin userLogin, Errors errors) {
+//
+//        if (errors.hasErrors()) {
+//            return ResponseResult.fail("입력값이 정확하지 않습니다.", ResponseError.of(errors.getAllErrors()));
+//        }
+//
+//        User user = null;
+//        try {
+//            user = userService.login(userLogin);
+//        } catch (BizException e) {
+//            return ResponseResult.fail(e.getMessage());
+//        }
+//        UserLoginToken userLoginToken = JWTUtils.createToken(user);
+//
+//        if (userLoginToken == null) {
+//            return ResponseResult.fail("JWT 생성에 실패하였습니다.");
+//        }
+//        return ResponseResult.success(userLoginToken);
+//    }
+
+    // Q84
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserLogin userLogin, Errors errors) {
+
+        log.info("로그인 함수 !!!!");
 
         if (errors.hasErrors()) {
             return ResponseResult.fail("입력값이 정확하지 않습니다.", ResponseError.of(errors.getAllErrors()));
@@ -35,11 +62,13 @@ public class ApiLoginController {
         try {
             user = userService.login(userLogin);
         } catch (BizException e) {
+            log.info("로그인 에러:" +e.getMessage());
             return ResponseResult.fail(e.getMessage());
         }
         UserLoginToken userLoginToken = JWTUtils.createToken(user);
 
         if (userLoginToken == null) {
+            log.info("JWT 생성 에러");
             return ResponseResult.fail("JWT 생성에 실패하였습니다.");
         }
         return ResponseResult.success(userLoginToken);
