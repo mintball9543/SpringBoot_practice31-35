dcd48c827a86d8df6b5f897114f9758e7e7592b7
mintball9543 <minseokcomputer@gmail.com>
2025-05-29 19:17:18 +0900

    Q87 RestTemplate을 이용한 공공데이터토털의 공공API연동하여 전국약국목록을 가져오는 API

--- src/main/java/com/example/jpa/extra/controller/ApiExtraController.java
+++ src/main/java/com/example/jpa/extra/controller/ApiExtraController.java
@@ -1,23 +1,28 @@
-package com.example.jpa.extra;
+package com.example.jpa.extra.controller;
 
+import com.example.jpa.common.model.ResponseResult;
+import com.example.jpa.extra.model.OpenApiResult;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
 
 @RequiredArgsConstructor
 @RestController
 public class ApiExtraController {
 
-    // Q86
+    // Q86~87
     @GetMapping("/api/extra/pharmacy")
-    public String pharmacy(){
+    public ResponseEntity<?> pharmacy() {
         String apiKey = "D3oa9VQ0e0tl8hkpoqTG9kLJWlUB%2FZmHdAZ0OWRPsS8PcY5Ft6PCx7pI2cgFkJS4z4tTs%2BHXcto50WqCrtlCPQ%3D%3D";
         String url = "https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?serviceKey=%s&pageNo=1&numOfRows=10";
 
@@ -36,6 +41,16 @@ public class ApiExtraController {
             e.printStackTrace();
         }
 
-        return apiResult;
+        OpenApiResult jsonResult = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        try {
+            jsonResult = objectMapper.readValue(apiResult, OpenApiResult.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseResult.success(jsonResult);
     }
+
 }
---
+++ src/main/java/com/example/jpa/extra/model/OpenApiResult.java
@@ -0,0 +1,34 @@
+package com.example.jpa.extra.model;
+
+import lombok.*;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OpenApiResult {
+
+    private OpenApiResultResponse response;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
